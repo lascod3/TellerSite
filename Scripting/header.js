7a3b1189c0e53fb04cd9bf4968941bc272c8a883
Lasana Kamara <lask0d3r@gmail.com>
2023-12-09 13:57:07 +0000

    Completes the sidebar on the dashboard

--- Scripting/header.js
+++ Scripting/header.js
@@ -1,10 +1,20 @@
-const menuBtn = document.querySelector("span.defaultIcon");
+const defaultIcon = document.querySelector("span.defaultIcon");
 const closeBtn = document.querySelector("span.closeIcon");
-
+const nav = document.querySelector("nav");
 const button = document.querySelector("header section button:first-child");
 
 button.addEventListener("click", (event) => {
   if (event.target.classList.contains("defaultIcon")) {
-    alert("Hello, world!");
+    defaultIcon.classList.replace("defaultIcon", "closeIcon");
+    closeBtn.classList.add("closeIconToggle");
+    closeBtn.animate({ transform: "rotateZ(90deg)" }, 100);
+
+    nav.classList.add("navToggle");
+    nav.animate({ opacity: ["0", "1"] }, 500);
+  } else {
+    defaultIcon.classList.replace("closeIcon", "defaultIcon");
+    defaultIcon.animate({ transform: "rotateZ(-90deg)" }, 100);
+    closeBtn.classList.remove("closeIconToggle");
+    nav.classList.remove("navToggle");
   }
 });
